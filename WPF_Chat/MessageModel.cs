0142611402448e175ce3ea666d35df0feaf46102
verhovensky <verhovenskyivan@gmail.com>
2022-09-07 10:52:11 +0300

    added message model

---
+++ WPF_Chat/MessageModel.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Chat;
+
+public class MessageModel
+{
+    public string Username { get; set; }
+    public string UsernameColor { get; set; }
+    public string imageSource { get; set; }
+    public string Message { get; set; }
+    public DateTime Time { get; set; }
+    public bool IsNativeOrigin { get; set; }
+    public bool? FirstMessage { get; set; }
+}
