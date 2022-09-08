64c05e217442a24d0174651633e7c4ba1cd29dae
verhovensky <verhovenskyivan@gmail.com>
2022-09-08 21:24:19 +0300

    added contact model

---
+++ WPF_Chat/ContactModel.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Chat;
+
+public class ContactModel
+{
+    public string Username { get; set; }
+    
+    public string ImageSource { get; set; }
+    
+    public ObservableCollection<MessageModel> Messages { get; set; }
+
+    public string LastMessage => Messages.Last().Message;
+}
